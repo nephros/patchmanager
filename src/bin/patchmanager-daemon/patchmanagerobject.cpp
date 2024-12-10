9ab8a27fd3e4a7f0f266455b5b5fcc7bf9c3e1ab
nephros <nemo@JollaC2>
2024-12-10 14:09:56 +0100

    Don't be a template class
     - Try a Q_OBJECT macro for a template class...
     - Don't be a template class
     - Become Object-based, add contructor
     - Implement a constructor

--- src/bin/patchmanager-daemon/patchmanagerobject.cpp
+++ src/bin/patchmanager-daemon/patchmanagerobject.cpp
@@ -3004,6 +3004,13 @@ void PatchManagerObject::setupFilter()
  *
  * \sa m_filter
  */
+
+PatchManagerFilter::PatchManagerFilter(QObject *parent, int maxCost )
+    : QObject(parent)
+    , QCache(maxCost)
+{
+}
+
 void PatchManagerFilter::setup()
 {
     static const char *etcList[] = {
